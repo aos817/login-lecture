04674da79316fb05910b965a0fcf90aa58905792
김대현 <aos817@naver.com>
2022-09-11 18:08:19 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -9,13 +9,26 @@ static #users={
  
  static getUsers(...fields){
     const users=this.#users;
-    const newUsers=fields.reduce((newUsers, field)=>{
-        if(users.hasOwnProperty(field)){
+    const newUsers = fields.reduce((newUsers, field)=>{
+        if (users.hasOwnProperty(field)){
             newUsers[field]=users[field];
-        }return newUsers;
-    },{})
+        }
+        return newUsers;
+    }, {});
     return newUsers;
  }
+
+ static getUserInfo(id) {
+    const users=this.#users;
+    const idx=users.id.indexOf(id);
+    const usersKeys=Object.keys(users); //=>[id,psword,name]
+    const userInfo= usersKeys.reduce((newUser,info)=>{
+        newUser[info]=users[info][idx];
+        return newUser;
+    }, {});
+
+    return userInfo;
+    }
 }
 
 module.exports=UserStorage;
