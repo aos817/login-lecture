fb761a325bf921368c9043995dcfefa26916a5d4
김대현 <aos817@naver.com>
2022-09-13 02:35:09 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -21,10 +21,16 @@ class User{
         return { success :false, msg: "존재하지 않는 아이디입니다" };
     }
     
-    register(){
+    async register(){
         const client = this.body;
-        UserStorage.save(client);  
-        return { success : true };
+        try{
+            const response=await UserStorage.save(client);  
+            return response;
+        }catch (err){
+            return {success :false,msg:err};
+        }
+        
+       
       
     }
 }
