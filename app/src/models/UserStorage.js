a6ec1bffdaf70dfcd365b36e800f05d2970358b7
김대현 <aos817@naver.com>
2022-09-13 01:43:27 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,13 @@
 "use strict";
 
+const fs=require("fs").promises;
+
+
 class UserStorage{
-static #users={
-    id:["kingsman","김대현","백엔드"],
-    psword:["1234","1234","123456"],
-    name:["김대현","문승기","동양미"],
- };
+
  
  static getUsers(...fields){
-    const users=this.#users;
+    // const users=this.#users;
     const newUsers = fields.reduce((newUsers, field)=>{
         if (users.hasOwnProperty(field)){
             newUsers[field]=users[field];
@@ -19,18 +18,28 @@ static #users={
  }
 
  static getUserInfo(id) {
-    const users=this.#users;
-    const idx=users.id.indexOf(id);
-    const usersKeys=Object.keys(users); //=>[id,psword,name]
-    const userInfo= usersKeys.reduce((newUser,info)=>{
-        newUser[info]=users[info][idx];
-        return newUser;
-    }, {});
+    return fs
+    .readFile("./src/databases/users.json")
+    .then((data) =>{
+        return this.#getUserInfo(data,id);
+    })
+    .catch(console.error);
 
-    return userInfo;
-    }
+}
+static #getUserInfo(data, id){
+        const users=JSON.parse(data);
+        const idx=users.id.indexOf(id);
+        const usersKeys=Object.keys(users); //=>[id,psword,name]
+        const userInfo= usersKeys.reduce((newUser,info)=>{
+            newUser[info]=users[info][idx];
+            return newUser;
+        }, {});
+    
+        return userInfo;
+     }
+
     static save(userInfo){
-        const users=this.#users;
+        // const users=this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,4 @@
-"user strict";
+"use strict";
 
 const User = require("../../models/User");
 
@@ -17,9 +17,9 @@ const output={
 
 
 const process={
-    login :(req,res)=>{
+    login : async (req,res)=>{
        const user= new User(req.body);
-       const response=user.login();
+       const response=await user.login();
        return res.json(response);
     },
     register:(req,res)=>{
