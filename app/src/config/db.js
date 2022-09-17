b9126a7941b51d35f3439662d57f0f899b774d79
김대현 <aos817@naver.com>
2022-09-17 18:48:47 +0900

    aws db 연결후 로그인 회원가입 구현

---
+++ app/src/config/db.js
@@ -0,0 +1,12 @@
+const mysql=require("mysql");
+
+const db =mysql.createConnection({
+    host:"kingsman.cechmfqjqioq.ap-northeast-2.rds.amazonaws.com",
+    user:"ci2022kingsman",
+    password:"2022kingsman",
+    database:"ci2022kingsman",
+    
+});
+db.connect();
+
+module.exports=db;
--- app/src/models/User.js
+++ app/src/models/User.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const { response } = require("express");
-const UserStorage = require("./UserStorage");
+const UserStorage =require("./UserStorage");
 
 class User{
     constructor(body){
@@ -10,7 +10,8 @@ class User{
 
     async login(){
         const client = this.body;
-        const {id,psword}=await UserStorage.getUserInfo(client.id);
+        try{
+       const {id,psword}= await UserStorage.getUserInfo(client.id);
 
         if (id) {
             if(id === client.id && psword ===client.psword) {
@@ -19,7 +20,10 @@ class User{
             return { success: false, msg: "비밀번호가 틀렸습니다." };
         }
         return { success :false, msg: "존재하지 않는 아이디입니다" };
+    }catch(err){
+        return { success:false, msg :err};
     }
+}
     
     async register(){
         const client = this.body;
